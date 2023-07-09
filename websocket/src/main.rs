6dc7642a8f81fab97c0aaa7c3b218a02966d49fd
aljaz-zupan <aljaz.zupan@gmail.com>
2023-07-09 12:59:06 +0200

    added serde, now some conflicts

--- websocket/src/main.rs
+++ websocket/src/main.rs
@@ -1,6 +1,21 @@
 use reqwest::Error;
+use serde::Deserialize;
 use std::time::Duration;
 use tokio::time;
+
+#[derive(Debug, Deserialize)]
+struct Match {
+    eventId: String,
+    homeTeam: String,
+    awayTeam: String,
+    vsTeams: String,
+    league: String,
+    restTime: String,
+    score: String,
+    period: String,
+    url: String,
+    hasSubscribe: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -11,12 +26,31 @@ async fn main() -> Result<(), Error> {
 }
 
 async fn fetch_json() -> Result<(), Error> {
-    let url: &str = "http://106.52.68.20/b365/soccer/test/oneHd2allEv/C1-G15?lang=en"; // Replace with your API endpoint
+    let url = "http://106.52.68.20/b365/soccer/test/allEv?lang=en"; // Updated API endpoint
 
-    let response: reqwest::Response = reqwest::get(url).await?;
-    let json = response.text().await?;
+    let response = reqwest::get(url).await?;
+    let matches: serde_json::Value = response.json().await?;
 
-    println!("Fetched JSON: {:?}", json);
-    println!("--------------------------");
+    if let Some(matches_json) = matches.as_object() {
+        let mut total_goals = 0;
+
+        for (_eventId, match_json) in matches_json {
+            let match_data: Match = serde_json::from_value(match_json.clone())?;
+
+            // Extract the integers from the score string
+            let goals: Vec<u32> = match_data
+                .score
+                .split('-')
+                .filter_map(|s| s.parse().ok())
+                .collect();
+
+            // Sum up the goals
+            let match_goals: u32 = goals.iter().sum();
+            total_goals += match_goals;
+        }
+
+        println!("Total Goals: {}", total_goals);
+    }
+
     Ok(())
 }
